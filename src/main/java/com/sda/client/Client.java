115c005dafea66381a0568b29dadd1505488d7fd
Mateusz Fiolek <fiolek.m@gmail.com>
2018-08-04 14:50:40 +0200

    clean to startet version

--- src/main/java/com/sda/client/Client.java
+++ src/main/java/com/sda/client/Client.java
@@ -1,29 +1,19 @@
 package com.sda.client;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class Client extends Application {
+public class Client {
     // host serwera
     private static final String HOSTNAME = "localhost";
     //    port serwera
     private static final Integer PORT = 8400;
 
-    Button button;
-
     public static void main(String[] args) throws IOException {
 
-        launch(args);
         //Tworzymy gniazdo do polaczenia z serwerem
         Socket socket = new Socket(HOSTNAME, PORT);
         //BufferedReader - obiekt do odczytu danych z serwera
@@ -43,21 +33,7 @@ public class Client extends Application {
         printWriter.close();
         reader.close();
         socket.close();
-    }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setTitle("Czat");
 
-        button = new Button();
-        button.setText("Click me");
-
-        StackPane layout = new StackPane();
-        layout.getChildren().add(button);
-
-        Scene scene = new Scene(layout, 300, 250);
-
-        primaryStage.setScene(scene);
-        primaryStage.show();
     }
 }
