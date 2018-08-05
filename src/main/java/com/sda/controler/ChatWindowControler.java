658bdfc4e3c43e7430fae7d26a988fef018bb44d
Mateusz Fiolek <fiolek.m@gmail.com>
2018-08-05 10:08:46 +0200

    add action to disconnect button

--- src/main/java/com/sda/controler/ChatWindowControler.java
+++ src/main/java/com/sda/controler/ChatWindowControler.java
@@ -2,12 +2,36 @@ package com.sda.controler;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ChatWindowControler {
     @FXML
+    private Button loginButton, disconnectButton;
+
+    @FXML
     private TextField chatMessageField, chatUsersField, chatAddMessageField;
 
-    public void handleDisconnectButtonAction(ActionEvent actionEvent) {
+    public void handleDisconnectButtonAction(ActionEvent actionEvent) throws IOException {
+        Stage stage;
+        Parent root;
+
+        if(actionEvent.getSource() == disconnectButton) {
+            stage = (Stage) disconnectButton.getScene().getWindow();
+            root = FXMLLoader.load(getClass().getResource("/fxml/LoginWindow.fxml"));
+        } else {
+            stage = (Stage) loginButton.getScene().getWindow();
+            root = FXMLLoader.load(getClass().getResource("/fxml/ChatWindow.fxml"));
+        }
+
+        Scene scene = new Scene(root, 400, 100);
+        stage.setScene(scene);
+        stage.show();
     }
 }
